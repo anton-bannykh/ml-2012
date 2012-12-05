da25646846ea51a2174227963f85c6962e500def
Aksenov239 <aksenov.vitaly@gmail.com>
2012-12-05 16:10:14 +0400

    Updating

--- vitaly.aksenov/HW01/src/Image.java
+++ vitaly.aksenov/HW01/src/Image.java
@@ -3,21 +3,24 @@ import java.io.IOException;
 
 public class Image {
 	int n, m;
-	byte[][] b;
-	byte[] vector;
+	int[][] b;
+	int[] vector;
+
+	static byte[] buffer = new byte[28 * 28];
 
 	public Image(int n, int m) {
 		this.n = n;
 		this.m = m;
-		b = new byte[n][m];
-		vector = new byte[n * m];
+		b = new int[n][m];
+		vector = new int[n * m];
 	}
 
 	public void read(DataInputStream in) throws IOException {
-		in.read(vector);
+		in.read(buffer);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				b[i][j] = vector[i * m + j];
+				b[i][j] = vector[i * m + j] = (buffer[i * m + j] < 0 ? 256 : 0)
+						+ buffer[i * m + j];
 			}
 		}
 	}
