35e1092ef9c4a93cc16d39867a6c124285baad2c
Ioann Volkov <volkov.ioann@gmail.com>
2012-12-12 13:03:36 +0400

    Fix stupid mistake. Add file with results

--- ioann.volkov/lab1.cpp
+++ ioann.volkov/lab1.cpp
@@ -6,7 +6,7 @@
 #include "images_holder.h"
 
 int f(ByteVector::const_iterator ib, std::vector<int> const& Q, int* result = 0) {
-    int res;
+    int res = 0;
     for (size_t i = 0; i < Q.size(); ++i) {
         res += Q[i] * (*ib);
         ++ib;
@@ -36,7 +36,7 @@ int main() {
     assert(trainIH.width() == testIH.width() && trainIH.height() == testIH.height());
 
     size_t N = trainIH.width() * trainIH.height();
-    size_t steps = 2;
+    size_t steps = 100;
 
     std::vector<int> Q[10];
     for (size_t z = 0; z < 10; ++z) {
@@ -45,41 +45,43 @@ int main() {
     }
 
     for (size_t j = 0; j < steps; ++j) {
-        std::cout << "Step " << j + 1 << ":" << std::flush;
-        boost::timer::auto_cpu_timer t;
+        {
+            std::cout << "Step " << j + 1 << ":" << std::flush;
+            boost::timer::auto_cpu_timer t;
 
-        for (size_t i = 0; i < trainIH.size(); ++i) {
-            for (size_t z = 0; z < 10; ++z) {
-                int correctRes = (trainIH(i) == z ? 1 : -1);
-                if (f(trainIH[i], Q[z]) != correctRes) {
-                    adjustQ(Q[z], correctRes, trainIH[i]);
+            for (size_t i = 0; i < trainIH.size(); ++i) {
+                for (size_t z = 0; z < 10; ++z) {
+                    int correctRes = (trainIH(i) == z ? 1 : -1);
+                    if (f(trainIH[i], Q[z]) != correctRes) {
+                        adjustQ(Q[z], correctRes, trainIH[i]);
+                    }
                 }
             }
         }
-    }
 
-    int errorsCount = 0;
-    {
-        std::cout << "Testing:" << std::flush;
-        boost::timer::auto_cpu_timer t;
+        int errorsCount = 0;
+        {
+            std::cout << "Testing:" << std::flush;
+            boost::timer::auto_cpu_timer t;
 
-        for (size_t i = 0; i < testIH.size(); ++i) {
-            int ans = -1;
-            int maxRes = 0;
-            for (size_t z = 0; z < 10; ++z) {
-                int res;
-                f(testIH[i], Q[z], &res);
-                if (res >= maxRes) {
-                    maxRes = res;
-                    ans = z;
+            for (size_t i = 0; i < testIH.size(); ++i) {
+                int ans = -1;
+                int maxRes = 0;
+                for (size_t z = 0; z < 10; ++z) {
+                    int res;
+                    f(testIH[i], Q[z], &res);
+                    if (res >= maxRes) {
+                        maxRes = res;
+                        ans = z;
+                    }
                 }
+                if (ans != testIH(i))
+                    ++errorsCount;
             }
-            if (ans != testIH(i))
-                ++errorsCount;
         }
-    }
 
-    std::cout << "Test error rate: " << static_cast<double>(errorsCount) / testIH.size() << std::endl << std::flush;
+        std::cout << "Test error rate: " << static_cast<double>(errorsCount * 100) / testIH.size() << "%" << std::endl << std::flush;
+    }
 
     return 0;
 }
