ff34a1f7e3d8726040d5c70347ed644d14825e8d
Eugene Luckyanets <leugenea@gmail.com>
2013-01-04 19:09:56 +0400

    Reader class optimizations

--- eugene.luckyanets/Reader/reader.py
+++ eugene.luckyanets/Reader/reader.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'eugene'
 
 import struct
 import gzip
 import numpy
+import os
 
 from exceptions import CountMismatchReaderException
 
 
-class Reader():
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+class MNISTReader():
     path = ''
     prefix = ''
     count = 0
@@ -17,8 +23,8 @@ class Reader():
     def __init__(self, path='../../data/mnist/', prefix='train'):
         self.path = path
         self.prefix = prefix
-        labelsResult = Reader.readLabels(path, prefix)
-        imagesResult = Reader.readImages(path, prefix)
+        labelsResult = MNISTReader.readLabels(path, prefix)
+        imagesResult = MNISTReader.readImages(path, prefix)
         if not labelsResult[1] == imagesResult[1]:
             raise CountMismatchReaderException(labelsResult[1], imagesResult[1])
         self.count = labelsResult[1]
@@ -27,14 +33,19 @@ class Reader():
 
     @classmethod
     def readLabels(cls, path='../../data/mnist/', prefix='train'):
-        filename = path + prefix + '-labels.idx1-ubyte.gz'
+        filename = os.path.join(ROOT_PATH, path + prefix + '-labels-idx1-ubyte.gz')
+        filename = os.path.normpath(filename)
         f = gzip.open(filename)
         content = f.read()
         f.close()
         [magic_number, count] = struct.unpack('!ll', content[0:8])
-        labels = []
+        labels1 = []
         for i in range(count):
-            labels.append(struct.unpack('!B', content[i + 8])[0])
+            labels1.append(struct.unpack('!B', content[i + 8])[0])
+        labels = numpy.array(struct.unpack('!' + 'B' * count, content[8:]))
+        for i in range(count):
+            if labels1[i] != labels[i]:
+                print i
         print 'Labels read'
         return [magic_number, count, labels]
 
@@ -43,17 +54,19 @@ class Reader():
         def normalize(a):
             return (float(a) - 128.0) / 128.0
 
-        filename = path + prefix + '-images-idx3-ubyte.gz'
+        filename = os.path.join(ROOT_PATH, path + prefix + '-images-idx3-ubyte.gz')
+        filename = os.path.normpath(filename)
         f = gzip.open(filename)
         content = f.read()
         f.close()
         [magic_number, count, row_len, col_len] = struct.unpack('!llll', content[0:16])
         images = []
         for i in range(count):
-            images.append(numpy.zeros((row_len, col_len)))
+            images.append(numpy.zeros((row_len, col_len), dtype=float))
+            unpacked = struct.unpack('!' + 'B' * row_len * col_len,
+                                     content[16 + i * row_len * col_len: 16 + (i + 1) * row_len * col_len])
             for row in range(row_len):
                 for col in range(col_len):
-                    images[i][row][col] = normalize(
-                        struct.unpack('!B', content[16 + i * row_len * col_len + row * row_len + col])[0])
+                    images[i][row][col] = normalize(unpacked[row * row_len + col])
         print 'Images read'
         return [magic_number, count, row_len, col_len, images]
